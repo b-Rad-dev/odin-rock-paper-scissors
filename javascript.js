775b9e42a0bfcaf5f9ca67d0bae74f7eb8e1b769
Brad Henson <bradhenson@gmail.com>
2022-01-26 21:15:31 -0500

    Updated Javascript.js to include function to play the game 5x, track score and determine a winner.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
-const playerSelection = "RocK";
-const computerSelection = computerPlay ();
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 // Function for computer to randomly select - rock, paper, scissors
 function computerPlay () {
@@ -13,24 +15,50 @@ function computerPlay () {
     }
 }
 
-console.log (playerSelection, computerSelection)
-
 //Function that plays a single round of Rock, Paper, Scissors
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase ();
     if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         return "You Win! Rock beats Scissors"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You Win! Paper beats Rock"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You Win! Scissors beats Paper"
     } else if (playerSelection === computerSelection) {
         return "Tie Game"
     } else {
+        computerScore++
         computerSelection = computerSelection.charAt(0).toUpperCase () + computerSelection.slice(1)
         playerSelection = playerSelection.charAt(0).toUpperCase () + playerSelection.slice(1)
         return "You Lose! " + computerSelection + " beats " + playerSelection 
     }
 }
 
-console.log (playRound(playerSelection, computerSelection))
+
+
+//Loop to play 5 Rounds
+for (let i=0; i<5; i++) {
+    let playerSelection = prompt("Pick Rock, Paper or Scissors")
+    playerSelection = playerSelection.toLowerCase ();
+    const computerSelection = computerPlay ();
+    console.log (playerSelection, computerSelection)
+    console.log (playRound(playerSelection, computerSelection))
+    console.log (playerScore, computerScore)
+}
+
+//Function to determine the Winner
+function winner (playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        return "WINNER!"
+    } else if (playerScore < computerScore) {
+        return "COMPUTER WINS"
+    } else {
+        return "TIE GAME"
+    }
+}
+
+console.log (winner(playerScore, computerScore))
+
+    
